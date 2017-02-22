9285935e360b0493cd0854b979e50128afa61cb0
GWHS Robotics Team 5507 <gwhsrobotics129@gmail.com>
2017-02-21 17:58:25 -0800

    huuuhhhh

--- src/org/usfirst/frc/team5507/robot/Robot.java
+++ src/org/usfirst/frc/team5507/robot/Robot.java
@@ -47,7 +47,7 @@ public class Robot extends IterativeRobot {
 	
 	/**
 	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
+	 * used for any initialization code. B Money is funny :)
 	 */
 	@Override
 	public void robotInit() {
