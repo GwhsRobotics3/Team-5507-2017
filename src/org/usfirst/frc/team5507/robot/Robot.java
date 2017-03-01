e574eceb6c291c51451ea2353cb79cd79133a602
GWHS Robotics Team 5507 <gwhsrobotics129@gmail.com>
2017-03-01 15:09:06 -0800

    Acceleration Code

--- src/org/usfirst/frc/team5507/robot/Robot.java
+++ src/org/usfirst/frc/team5507/robot/Robot.java
@@ -15,7 +15,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * Check out all our code. You won't understand it. 
- * Howard He wrote the first half, Legina Chen wrote the
+ * Master He wrote the first half, Legina Chen wrote the																															I should be the new software leader.
  * second half, but Meau Bonton is the "real" software leader.
  * Call (415) 400-6078 for help. Good luck.
  * 
@@ -32,9 +32,11 @@ public class Robot extends IterativeRobot {
 	RobotDrive myRobot = new RobotDrive(0, 1, 2, 3);
 	Spark climber = new Spark(4);
 	Timer timer = new Timer();
+	Timer tAcceleration = new Timer();
 	Timer timerMotor = new Timer();
 	Timer timerAuto = new Timer();
 	Timer timerPneumatics = new Timer();
+	double maxAcceleration = 0.;
 	Compressor c = new Compressor(0);
 	int caseAuto = 0;
 	int state = 0;
@@ -116,7 +118,7 @@ public class Robot extends IterativeRobot {
 	 * method to get horizontal offset from center
 	 * @return a double representing horizontal offset from center
 	 */
-	public double getCameraHorizontal(){ //image is rotated 270 degrees because camera is placed sideways
+	public double getCameraHorizontal() { //image is rotated 270 degrees because camera is placed sideways
 		double cameraOffsetFromCenter = 0;
 		double temp_centerY = camera.getCenterY();
 		double temp_imgHeight = (double)camera.getImgHeight();
@@ -129,7 +131,7 @@ public class Robot extends IterativeRobot {
 	 * method to get vertical offset from center
 	 * @return a double representing vertical offset from center
 	 */
-	public double getCameraVertical(){
+	public double getCameraVertical() {
 		double cameraOffsetFromCenter = 0;
 		double temp_centerX = camera.getCenterX();
 		double temp_imgWidth = (double)camera.getImgWidth();
@@ -141,7 +143,7 @@ public class Robot extends IterativeRobot {
 	/**
 	 *This is code for the "Drive Straight" option in autonomous code.
 	 */
-	public void autonomousDriveStraight(){
+	public void autonomousDriveStraight() {
 		switch (autonomousState){
 		case 0:
 			myRobot.mecanumDrive_Cartesian(0.09, -1, 0, 0);
@@ -161,7 +163,7 @@ public class Robot extends IterativeRobot {
 	 * @param pixels number of pixels
 	 * @return a double representing inches from wall and robot 
 	 */
-	public double getDistanceToWallInInches(double pixels){
+	public double getDistanceToWallInInches(double pixels) {
 		return 0.0039494*pixels*pixels - 2.3615*pixels + 361.92;
 	}
 	
@@ -169,30 +171,30 @@ public class Robot extends IterativeRobot {
 	 * Code that gets a value from -1 to 1 representing x value distance from center.
 	 * @return value from -1 to 1 representing offset from center
 	 */
-	public double getCameraOffsetFromCenter(){ //image is rotated 270 degrees
+	public double getCameraOffsetFromCenter() { //image is rotated 270 degrees
 		return this.getCameraHorizontal();
 	}
 	
 	/**
 	 * Use this autonomous mode when starting from the center
 	 */
-	public void autonomousCamera(){
+	public void autonomousCamera() {
 		SmartDashboard.putNumber("Distance from wall: ", this.getDistanceToWallInInches(camera.getCenterX()));
 		SmartDashboard.putNumber("Autonomous State: ", autonomousState);
 		double xDrive = 0;
 		double yDrive = 0;
 		double rotateDrive = 0;
 		
-		switch(autonomousState){
+		switch(autonomousState) {
 		case 0: //drive forward until we get to the gear peg
-			if(this.getCameraOffsetFromCenter()<-0.12){
+			if (this.getCameraOffsetFromCenter()<-0.12) {
 				xDrive = -0.4;
 			}
-			else if(this.getCameraOffsetFromCenter()>0.12){
+			else if (this.getCameraOffsetFromCenter()>0.12) {
 				xDrive = 0.4; //right
 			}
 			yDrive = -0.25; //forward
-			if(this.getDistanceToWallInInches(camera.getCenterX()) < 12){
+			if (this.getDistanceToWallInInches(camera.getCenterX()) < 12) {
 				autonomousState = 1;
 				timerAuto.reset();
 			}
@@ -281,14 +283,14 @@ public class Robot extends IterativeRobot {
 	/**
 	 * method to autonomously drive robot if it starts on the left side 
 	 */
-	public void autonomousCameraLeftSide(){
+	public void autonomousCameraLeftSide() {
 		SmartDashboard.putNumber("Distance from wall: ", this.getDistanceToWallInInches(camera.getCenterX()));
 		SmartDashboard.putNumber("Autonomous State: ", autonomousState);
 		double xDrive = 0;
 		double yDrive = 0;
 		double rotateDrive = 0;
 		
-		switch(autonomousState){
+		switch(autonomousState) {
 		case 0:
 			xDrive = -0.5;
 			yDrive = 0;
@@ -387,7 +389,7 @@ public class Robot extends IterativeRobot {
 	/**
 	 * method to autonomously drive robot if it starts on the right side
 	 */
-	public void autonomousCameraRightSide(){
+	public void autonomousCameraRightSide() {
 		SmartDashboard.putNumber("Distance from wall: ", this.getDistanceToWallInInches(camera.getCenterX()));
 		SmartDashboard.putNumber("Autonomous State: ", autonomousState);
 		double xDrive = 0;
@@ -468,7 +470,7 @@ public class Robot extends IterativeRobot {
 	/**
 	 * method to autonomously drive robot in a box
 	 */
-	public void autonomousBox(){
+	public void autonomousBox() {
 		if(timerAuto.get()>0 && timerAuto.get()<0.25){
 			myRobot.mecanumDrive_Cartesian(0.09, -1, 0, 0); //FORWARD
 			}
@@ -501,16 +503,16 @@ public class Robot extends IterativeRobot {
 		this.openGearHolder();
 	}
 		
-	public void closeGearHolder(){
+	public void closeGearHolder() {
 		solenoid1.set(DoubleSolenoid.Value.kReverse);
 		solenoid2.set(DoubleSolenoid.Value.kReverse);
-		SmartDashboard.putString("Gear holder: ", "Open");
+		SmartDashboard.putString("Gear holder: ", "Closed");
 	}
 	
-	public void openGearHolder(){
+	public void openGearHolder() {
 		solenoid1.set(DoubleSolenoid.Value.kForward);
 		solenoid2.set(DoubleSolenoid.Value.kForward);
-		SmartDashboard.putString("Gear holder: ", "Closed");
+		SmartDashboard.putString("Gear holder: ", "Opened");
 	}
 
 	/**
@@ -518,7 +520,6 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void teleopPeriodic() {
-		
 //		if(stick.getRawButton(3)){
 //			relay.set(true);
 //		}
@@ -529,36 +530,51 @@ public class Robot extends IterativeRobot {
 		double yDrive = 0.;
 		double rotateDrive = 0.0;
 		
-		if(Math.abs(stick.getX())>0.25 ) {
-			xDrive = stick.getX()*0.75;
+		if (Math.abs(stick.getX()) > 0.25 ) {
+			tAcceleration.reset();
+			tAcceleration.start();
 			c.stop(); //Need to test to stop compressor
+//			if(tAcceleration.get() > 0 && tAcceleration.get() < 0.5){ //Acceleration code
+//				maxAcceleration = 0.25;
+//			}
+//			if(tAcceleration.get() > 0.5 && tAcceleration.get() < 1.){
+//				maxAcceleration = 0.5;
+//			}
+//			if(tAcceleration.get() > 1. && tAcceleration.get() < 1.5){
+//				maxAcceleration = 0.75;
+//			}
+//			if(tAcceleration.get() > 1.5 && tAcceleration.get() < 2){
+//				maxAcceleration = 1.;
+//			}
+			xDrive = stick.getX()*(Math.min(1., tAcceleration.get()/2.0));
 		}
-		else{
+		else {
 			c.start(); //Need to test to start again
 		}
-		if(Math.abs(stick.getY())>0.25 ) {
+		if (Math.abs(stick.getY()) > 0.25 ) {
 			yDrive = stick.getY();
 		}
-		if((Math.abs(stick.getRawAxis(2)- stick.getRawAxis(3))>0.1 )) {
+		if ((Math.abs(stick.getRawAxis(2) - stick.getRawAxis(3)) > 0.1 )) {
 			rotateDrive = stick.getRawAxis(2) - stick.getRawAxis(3);
 		}
 		
 		//computer assisted line-up
-		if(stick.getRawButton(6))
+		if (stick.getRawButton(6))
 		{
-			if(this.getCameraOffsetFromCenter()<-0.05){
+			if (this.getCameraOffsetFromCenter() < -0.05) {
 				xDrive = -0.4;
 			}
-			else if(this.getCameraOffsetFromCenter()>0.05){
+			else if (this.getCameraOffsetFromCenter() > 0.05) {
 				xDrive = 0.4; //right
 			}
 			SmartDashboard.putString("Assist Mode: ", "on");
 		}
-		else{
+		else {
 			SmartDashboard.putString("Assist Mode: ", "off");
 		}
 		
-		myRobot.mecanumDrive_Cartesian(xDrive*0.75, yDrive*0.75, -rotateDrive,0); //0.75 celing on max speed
+		myRobot.mecanumDrive_Cartesian(xDrive * 0.75, yDrive * 0.75, -rotateDrive,0); //0.75 ceiling on max speed
+		
 		
 		SmartDashboard.putNumber("xDrive: ", xDrive);
 		SmartDashboard.putNumber("yDrive: ", yDrive);
@@ -568,23 +584,23 @@ public class Robot extends IterativeRobot {
 		SmartDashboard.putNumber("imgHeight: ", camera.getImgHeight());
 
 		// control Climber code
-		if(stick.getRawButton(1)) {// Button 1 is A
+		if (stick.getRawButton(1)) {// Button 1 is A
 			climber.set(1);
 			SmartDashboard.putString("Climber Direction : ", "down");
 		}
-		else if(stick.getRawButton(2) ) { // button 2 is B 
+		else if (stick.getRawButton(2) ) { // button 2 is B 
 			climber.set(-1);
 			SmartDashboard.putString("Climber Direction : ", "up");
 		}
-		else{
+		else {
 			climber.set(0);
 		}
 		
-		if(timerPneumatics.get() == 0){
+		if (timerPneumatics.get() == 0){
 			timerPneumatics.start();
 		}
 		
-		switch(state){
+		switch(state) {
 			case 0 : // Open Listen for Button
 				openGearHolder();
 				if(stick.getRawButton(5)){
